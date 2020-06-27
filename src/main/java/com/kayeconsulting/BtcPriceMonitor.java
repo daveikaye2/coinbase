b3f34c68474aa0883481cf449434c7e06d95d9a4
David Kaye <davekaye77@gmail.com>
2020-06-27 15:00:05 -0400

    use xchange libraries for interaction w/ exchanges.

--- src/main/java/com/kayeconsulting/BtcPriceMonitor.java
+++ src/main/java/com/kayeconsulting/BtcPriceMonitor.java
@@ -1,5 +1,11 @@
 package com.kayeconsulting;
 
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.coinbasepro.CoinbaseProExchange;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.Ticker;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -7,21 +13,25 @@ import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 public class BtcPriceMonitor {
     public void monitor() {
-        Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target("https://api.pro.coinbase.com");
-        WebTarget tickerWebTarget = webTarget.path("products/BTC-USD/ticker");
-        Builder invocationBuilder = tickerWebTarget.request(MediaType.APPLICATION_JSON);
-        Ticker ticker = invocationBuilder.get(Ticker.class);
-        System.out.println(ticker);
+        Exchange coinbasePro = ExchangeFactory.INSTANCE.createExchange(CoinbaseProExchange.class);
+        try {
+            Ticker ticker = coinbasePro.getMarketDataService().getTicker(CurrencyPair.BTC_USD);
+            System.out.println(ticker);
 
-        String slackWebhookUrlStr = System.getenv("SLACK_WEB_HOOK_MISC_POST_URL");
-        WebTarget slackWebhookUrl = client.target(slackWebhookUrlStr);
-        Builder slackInvocationBuilder = slackWebhookUrl.request(MediaType.APPLICATION_JSON);
-        String message = "{'text':'"+ticker+"'}";
-        Response post = slackInvocationBuilder.post(Entity.entity(message, MediaType.APPLICATION_JSON));
-        System.out.println(post);
+            String slackWebhookUrlStr = System.getenv("SLACK_WEB_HOOK_MISC_POST_URL");
+
+            //Slack notification
+            Client client = ClientBuilder.newClient();
+            WebTarget slackWebhookUrl = client.target(slackWebhookUrlStr);
+            Builder slackInvocationBuilder = slackWebhookUrl.request(MediaType.APPLICATION_JSON);
+            String message = "{'text':'"+ ticker +"'}";
+            Response post = slackInvocationBuilder.post(Entity.entity(message, MediaType.APPLICATION_JSON));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
